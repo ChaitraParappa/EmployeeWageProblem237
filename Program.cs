bc4f2ca1a6e0f0006ba0adb94b7bd5b2a2d79d21
Chaitra P <chaitra.p@bp.com>
2022-12-14 08:35:17 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+
+    }
+}
